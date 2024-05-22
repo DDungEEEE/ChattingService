7b4796f4f8d2ecbda536e66f81cda31df3a9bbae
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-05-22 12:28:26 +0900

    create Stomp Chatting Myself

--- src/main/java/chatting/service/config/WebSocketConfig.java
+++ src/main/java/chatting/service/config/WebSocketConfig.java
@@ -11,15 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("*")
-                .withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/stomp")
+                .setAllowedOrigins("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setApplicationDestinationPrefixes("/sub");
+        registry.enableSimpleBroker("/pub");
     }
 }
